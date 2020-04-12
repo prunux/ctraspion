1bd93796201cfe58d5782f0b88425849bb862a90
Roman Plessl <roman.plessl@prunux.ch>
2020-04-12 20:33:05 +0000

    changed defaults for my setup: - on Ubuntu 20.04 (x86-64) - keep language setting done by Ubuntu Installer - changed to another user (localadmin)

--- install2.sh
+++ install2.sh
@@ -11,11 +11,10 @@ set -e
 
 WD=$(pwd)
 LOG=/var/log/raspion.log
-NEWLANG=de_DE.UTF-8
 [[ -f .version ]] && source ./.version || VER=$(git rev-parse --short HEAD)
 source ./.defaults
 sudo touch $LOG
-sudo chown pi:pi $LOG
+sudo chown $USER:adm $LOG
 
 trap 'error_report $LINENO' ERR
 error_report() {
@@ -23,12 +22,9 @@ error_report() {
 }
 
 echo "==> Einrichtung des c't-Raspion ($VER)" | tee -a $LOG
-echo "* Wifi einschalten" | tee -a $LOG
-rfkill unblock wifi >> $LOG 2>&1
 
 echo "* Hilfspakete hinzufügen, Paketlisten aktualisieren" | tee -a $LOG
 sudo dpkg -i $WD/debs/raspion-keyring_2019_all.deb  >> $LOG 2>&1
-sudo dpkg -i $WD/debs/apt-ntop_1.0.190416-469_all.deb  >> $LOG 2>&1
 # the former calls apt-get update in postinst
 
 echo "* Firewallregeln vorbereiten, Module laden" | tee -a $LOG
@@ -50,53 +46,33 @@ echo "* Firewall-Regeln speichern ..." | tee -a $LOG
 sudo netfilter-persistent save >> $LOG 2>&1
 
 echo "* Raspbian aktualisieren ..." | tee -a $LOG
-sudo apt-get -y --allow-downgrades dist-upgrade >> $LOG 2>&1
-
-echo "* Raspbian Sprachanpassungen ..." | tee -a $LOG
-sudo debconf-set-selections debconf/keyboard-configuration >> $LOG 2>&1
-sudo cp files/keyboard /etc/default >> $LOG 2>&1
-sudo dpkg-reconfigure -fnoninteractive keyboard-configuration >> $LOG 2>&1
-
-sudo sed -i -e "/^[# ]*$NEWLANG/s/^[# ]*//" /etc/locale.gen  >> $LOG 2>&1
-sudo dpkg-reconfigure -fnoninteractive locales >> $LOG 2>&1
-sudo update-locale LANG=$NEWLANG >> $LOG 2>&1
-
-sudo debconf-set-selections debconf/tzdata >> $LOG 2>&1
-sudo ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime >> $LOG 2>&1
-sudo cp files/timezone /etc >> $LOG 2>&1
-sudo dpkg-reconfigure -fnoninteractive tzdata >> $LOG 2>&1
+sudo apt-get -y dist-upgrade >> $LOG 2>&1
 
 echo "* Pakete installieren ..." | tee -a $LOG
-sudo apt-get install -y --allow-downgrades raspion --no-install-recommends >> $LOG 2>&1
+sudo apt-get install -y raspion --no-install-recommends >> $LOG 2>&1
 
 echo "* Softwaregrundkonfiguration ..." | tee -a $LOG
-sudo usermod -a -G wireshark pi >> $LOG 2>&1
-sudo usermod -a -G www-data pi >> $LOG 2>&1
+sudo usermod -a -G wireshark $LOCALGROUP >> $LOG 2>&1
+sudo usermod -a -G www-data $LOCALGROUP >> $LOG 2>&1
 sudo cp $WD/files/ntopng.conf /etc/ntopng >> $LOG 2>&1
 sudo sed -i "s/^-m=#IPv4NET#/-m=$IPv4NET/" /etc/ntopng/ntopng.conf >> $LOG 2>&1
-sudo cp $WD/files/interfaces /etc/network >> $LOG 2>&1
-sudo sed -i "s/^  address #IPv4HOST#/  address $IPv4HOST/" /etc/network/interfaces >> $LOG 2>&1
-sudo sed -i "s/^  address #IPv6HOST#/  address $IPv6HOST/" /etc/network/interfaces >> $LOG 2>&1
-sudo cp $WD/files/hostapd.conf /etc/hostapd >> $LOG 2>&1
-sudo sed -i "s/^ssid=#SSID#/ssid=$SSID/" /etc/hostapd/hostapd.conf >> $LOG 2>&1
+sudo sed -i "s/^-m=#INTERFACE#/-m=$INTERFACE/" /etc/ntopng/ntopng.conf >> $LOG 2>&1
+#sudo sed -i "s/^  address #IPv4HOST#/  address $IPv4HOST/" /etc/network/interfaces >> $LOG 2>&1
+#sudo sed -i "s/^  address #IPv6HOST#/  address $IPv6HOST/" /etc/network/interfaces >> $LOG 2>&1
 sudo cp $WD/files/ipforward.conf /etc/sysctl.d >> $LOG 2>&1
 sudo cp $WD/files/hostname /etc/ >> $LOG 2>&1
-sudo cp $WD/files/raspion-sudo /etc/sudoers.d/ >> $LOG 2>&1
+sudo cp $WD/files/spion-sudo /etc/sudoers.d/ >> $LOG 2>&1
 sudo cp $WD/files/radvd.conf /etc/ >> $LOG 2>&1
 sudo sed -i "s/^  RDNSS #IPv6HOST#/  RDNSS $IPv6HOST/" /etc/radvd.conf >> $LOG 2>&1
 sudo mkdir -p /root/.mitmproxy >> $LOG 2>&1
-sudo cp $WD/files/config.yaml /root/.mitmproxy >> $LOG 2>&1
-mkdir -p /home/pi/.config/wireshark >> $LOG 2>&1
-cp $WD/files/recent /home/pi/.config/wireshark >> $LOG 2>&1
-cp $WD/files/preferences_wireshark /home/pi/.config/wireshark/preferences >> $LOG 2>&1
-sudo cp $WD/files/settings.ini /etc/gtk-3.0 >> $LOG 2>&1
+sudo cp $WD/files/mitmproxy-config.yaml /root/.mitmproxy >> $LOG 2>&1
+mkdir -p /home/$LOCALUSER/.config/wireshark >> $LOG 2>&1
+cp $WD/files/wireshark_config /home/$LOCALUSER/.config/wireshark >> $LOG 2>&1
+cp $WD/files/wireshark_preferences /home/$LOCALUSER/.config/wireshark/preferences >> $LOG 2>&1
+sudo cp $WD/files/gtk-settings.ini /etc/gtk-3.0 >> $LOG 2>&1
 sudo cp -f $WD/files/shellinabox /etc/default >> $LOG 2>&1
 cd /usr/lib/python3/dist-packages/mitmproxy/addons/onboardingapp/static >> $LOG 2>&1
 sudo ln -sf /usr/share/fonts-font-awesome fontawesome >> $LOG 2>&1
-PW=$(pwgen --ambiguous 9)
-sudo -s <<HERE
-echo "wpa_passphrase=$PW" >> /etc/hostapd/hostapd.conf
-HERE
 
 echo "* systemd-Units vorbereiten ..." | tee -a $LOG
 sudo systemctl enable mitmweb.service >> $LOG 2>&1
@@ -112,7 +88,7 @@ sudo ln -sf ../conf-available/10-proxy.conf 10-proxy.conf >> $LOG 2>&1
 sudo cp $WD/files/10-dir-listing.conf . >> $LOG 2>&1
 sudo -s <<HERE
 echo '\$SERVER["socket"] == ":81" {
-        server.document-root = "/home/pi/public_html"
+        server.document-root = "/home/localadmin/public_html"
         dir-listing.encoding = "utf-8"
         \$HTTP["url"] =~ "^/caps(\$|/)" {
             dir-listing.activate = "enable" 
@@ -125,9 +101,9 @@ echo '\$SERVER["socket"] == ":81" {
         }
 }' > /etc/lighttpd/conf-enabled/20-extport.conf
 HERE
-sudo chmod g+s /home/pi/public_html/caps >> $LOG 2>&1
-sudo chmod 777 /home/pi/public_html/caps >> $LOG 2>&1
-sudo chgrp www-data /home/pi/public_html/caps >> $LOG 2>&1
+sudo chmod g+s /home/localadmin/public_html/caps >> $LOG 2>&1
+sudo chmod 777 /home/localadmin/public_html/caps >> $LOG 2>&1
+sudo chgrp www-data /home/localadmin/public_html/caps >> $LOG 2>&1
 
 echo "* Pi-hole installieren ..." | tee -a $LOG
 if ! id pihole >/dev/null 2>&1; then
@@ -141,6 +117,7 @@ sudo sed -i "s/IPV6_ADDRESS=#IPv6HOST#/IPV6_ADDRESS=$IPv6HOST/" /etc/pihole/setu
 sudo sed -i "s/DHCP_ROUTER=#IPv4HOST#/DHCP_ROUTER=$IPv4HOST/" /etc/pihole/setupVars.conf >> $LOG 2>&1
 sudo sed -i "s/DHCP_START=#DHCPv4START#/DHCP_START=$DHCPv4START/" /etc/pihole/setupVars.conf >> $LOG 2>&1
 sudo sed -i "s/DHCP_END=#DHCPv4END#/DHCP_END=$DHCPv4END/" /etc/pihole/setupVars.conf >> $LOG 2>&1
+sudo sed -i "s/PIHOLE_INTERFACE=#INTERFACE#/PIHOLE_INTERFACE=$INTERFACE/" /etc/pihole/setupVars.conf >> $LOG 2>&1
 sudo -s <<HERE
 curl -sSL https://install.pi-hole.net | bash /dev/stdin --unattended >> $LOG 2>&1
 HERE
@@ -150,7 +127,7 @@ sudo chattr -f +i /etc/init.d/pihole-FTL >> $LOG 2>&1
 sudo systemctl daemon-reload >> $LOG 2>&1
 sudo systemctl restart pihole-FTL >> $LOG 2>&1
 sudo pihole -f restartdns >> $LOG 2>&1
-sudo cp $WD/files/hosts /etc/ >> $LOG 2>&1
+# sudo cp $WD/files/hosts /etc/ >> $LOG 2>&1
 
 echo "==> Installation des c't-Raspion erfolgreich abgeschlossen." | tee -a $LOG
 echo ""
