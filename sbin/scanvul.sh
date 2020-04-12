c787da3d5b06f670623961d131f9502a6d1ebf7d
Roman Plessl <roman.plessl@prunux.ch>
2020-04-12 20:05:21 +0000

    changed user from 'pi' to 'localadmin'

--- sbin/scanvul.sh
+++ sbin/scanvul.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 nmap -oX /tmp/$$_nmap.xml --script vuln $1
-xsltproc /tmp/$$_nmap.xml -o ~pi/public_html/scans/$1_$$_nmap.html
-chown pi:pi ~pi/public_html/scans/$1_$$_nmap.html
+xsltproc /tmp/$$_nmap.xml -o ~localadmin/public_html/scans/$1_$$_nmap.html
+chown localadmin:localadmin ~localadmin/public_html/scans/$1_$$_nmap.html
 
